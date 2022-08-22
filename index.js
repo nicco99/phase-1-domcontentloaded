7e1c2c71230c383610fba064cb04a1cc5bd6b275
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-23 00:24:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",changeFunc)
+
+function changeFunc() {
+    let p1 = document.getElementById("text");
+    p1.textContent = "This is really cool!"
+}
